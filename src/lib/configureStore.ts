e1da7286a2bde38733da032b312567bd67b0be51
Luca <duvet86@gmail.com>
2018-08-26 15:58:52 +0800

    Working.

--- src/lib/configureStore.ts
+++ src/lib/configureStore.ts
@@ -6,7 +6,7 @@ import history from "lib/history";
 import rootEpic from "rootEpic";
 import rootReducer from "rootReducer";
 
-const epicMiddleware = createEpicMiddleware<Action, Action, any>(); // Replac any with rootState.
+const epicMiddleware = createEpicMiddleware<Action, Action, any>(); // Fix me: Replace any with rootState.
 const browserRouterMiddleware = routerMiddleware(history);
 
 const middleware = [epicMiddleware, browserRouterMiddleware];
