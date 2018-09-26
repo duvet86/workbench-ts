140b6d1cfab14e7d7aa8a6817326bbf2741bae2f
Luca <duvet86@gmail.com>
2018-09-26 19:45:08 +0800

    Working.

--- src/sidebar/SideBar.tsx
+++ src/sidebar/SideBar.tsx
@@ -1,9 +1,11 @@
 import React, { SFC } from "react";
 import { Location } from "history";
+import { Link } from "react-router-dom";
 
 import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
 
 import Drawer from "@material-ui/core/Drawer";
+import Button, { ButtonProps } from "@material-ui/core/Button";
 
 import NavigationTabsContainer from "sidebar/navigationTabs/NavigationTabsContainer";
 import SideBarBodyContainer from "sidebar/SideBarBodyContainer";
@@ -27,8 +29,25 @@ const styles = createStyles({
   drawerClosed: {
     ...drawerPaperStyles,
     width: 0
+  },
+  buttonContainer: {
+    margin: 10,
+    display: "flex",
+    justifyContent: "space-evenly"
   }
 });
+
+const nePagebuilderLink = ({ className, children }: ButtonProps) => (
+  <Link className={className} to="/pagebuilder/new">
+    {children}
+  </Link>
+);
+
+const newWorkbenchLink = ({ className, children }: ButtonProps) => (
+  <Link className={className} to="/workbench/new">
+    {children}
+  </Link>
+);
 
 const SideBar: SFC<IProps> = ({ classes, open, ...props }) => (
   <Drawer
@@ -38,6 +57,14 @@ const SideBar: SFC<IProps> = ({ classes, open, ...props }) => (
     variant="persistent"
     open={open}
   >
+    <div className={classes.buttonContainer}>
+      <Button variant="outlined" size="medium" component={nePagebuilderLink}>
+        New Page
+      </Button>
+      <Button variant="outlined" size="medium" component={newWorkbenchLink}>
+        New Workbench
+      </Button>
+    </div>
     <NavigationTabsContainer {...props} />
     <SideBarBodyContainer {...props} />
   </Drawer>
