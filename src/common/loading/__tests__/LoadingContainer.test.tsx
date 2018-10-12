47f3ab0943d33c0143275342d34e05b51476b910
Luca Marangon <Luca_Marangon@Trimble.com>
2018-10-12 17:01:54 +0800

    Working.

--- src/common/loading/__tests__/LoadingContainer.test.tsx
+++ src/common/loading/__tests__/LoadingContainer.test.tsx
@@ -8,41 +8,41 @@ import BaseLoading from "common/loading/BaseLoading";
 jest.useFakeTimers();
 
 describe("<LoadingContainer />", () => {
-  // it("Component with error shows error message.", () => {
-  //   const error = {
-  //     error: "ERROR"
-  //   };
-  //   const component = create(
-  //     <LoadingContainer isLoading={true} error={error}>
-  //       <div>Child</div>
-  //     </LoadingContainer>
-  //   );
+  it("Component with error shows error message.", () => {
+    const error = {
+      error: "ERROR"
+    };
+    const component = create(
+      <LoadingContainer isLoading={true} error={error}>
+        <div>Child</div>
+      </LoadingContainer>
+    );
 
-  //   const instance = component.root;
+    const instance = component.root;
 
-  //   const childComponent = instance.find(el => {
-  //     return el.children && el.children[0] === JSON.stringify(error);
-  //   });
-  //   expect(childComponent).toBeDefined();
-  // });
+    const childComponent = instance.find(el => {
+      return el.children && el.children[0] === JSON.stringify(error);
+    });
+    expect(childComponent).toBeDefined();
+  });
 
-  // it("Component not loading show children.", () => {
-  //   const component = create(
-  //     <LoadingContainer isLoading={false}>
-  //       <div>Child</div>
-  //     </LoadingContainer>
-  //   );
+  it("Component not loading show children.", () => {
+    const component = create(
+      <LoadingContainer isLoading={false}>
+        <div>Child</div>
+      </LoadingContainer>
+    );
 
-  //   const instance = component.root;
+    const instance = component.root;
 
-  //   expect(instance.instance.state.pastDelay).toStrictEqual(false);
-  //   expect(instance.children.length).toEqual(1);
+    expect(instance.instance.state.pastDelay).toStrictEqual(false);
+    expect(instance.children.length).toEqual(1);
 
-  //   const childComponent = instance.find(el => {
-  //     return el.children && el.children[0] === "Child";
-  //   });
-  //   expect(childComponent).toBeDefined();
-  // });
+    const childComponent = instance.find(el => {
+      return el.children && el.children[0] === "Child";
+    });
+    expect(childComponent).toBeDefined();
+  });
 
   it("Component not loading and outside delay shows children.", () => {
     const component = create(
@@ -64,7 +64,7 @@ describe("<LoadingContainer />", () => {
     });
     expect(childComponent).toBeDefined();
 
-    // HERE
+    // Rerender component, test shouldComponentUpdate.
     const renderSpy = jest.spyOn(instance.instance, "render");
     component.update(
       <LoadingContainer isLoading={true}>
@@ -78,39 +78,50 @@ describe("<LoadingContainer />", () => {
 
     jest.runAllTimers();
 
-    // expect(renderSpy).not.toHaveBeenCalled();
-
     const loadingComponent = instance.findByType(BaseLoading);
     expect(loadingComponent).toBeDefined();
   });
 
-  // it("Component loading within delay return null.", () => {
-  //   const component = create(
-  //     <LoadingContainer isLoading={true}>
-  //       <div>Child</div>
-  //     </LoadingContainer>
-  //   );
+  it("Component loading within delay return null.", () => {
+    const component = create(
+      <LoadingContainer isLoading={true}>
+        <div>Child</div>
+      </LoadingContainer>
+    );
 
-  //   const instance = component.root;
+    const instance = component.root;
 
-  //   expect(instance.instance.state.pastDelay).toStrictEqual(false);
-  //   expect(component.toJSON()).toBeNull();
-  // });
+    expect(instance.instance.state.pastDelay).toStrictEqual(false);
+    expect(component.toJSON()).toBeNull();
+  });
 
-  // it("Component loading outside delay shows spinner.", () => {
-  //   const component = create(
-  //     <LoadingContainer isLoading={true}>
-  //       <div>Child</div>
-  //     </LoadingContainer>
-  //   );
+  it("Component loading outside delay shows spinner.", () => {
+    const component = create(
+      <LoadingContainer isLoading={true}>
+        <div>Child</div>
+      </LoadingContainer>
+    );
 
-  //   // Fast-forward until all timers have been executed.
-  //   jest.runAllTimers();
+    // Fast-forward until all timers have been executed.
+    jest.runAllTimers();
 
-  //   const instance = component.root;
+    const instance = component.root;
 
-  //   expect(instance.instance.state.pastDelay).toStrictEqual(true);
-  //   const childComponent = instance.findByType(BaseLoading);
-  //   expect(childComponent).toBeDefined();
-  // });
+    expect(instance.instance.state.pastDelay).toStrictEqual(true);
+    const childComponent = instance.findByType(BaseLoading);
+    expect(childComponent).toBeDefined();
+  });
+
+  it("Component unmounted clears timeout.", () => {
+    const component = create(
+      <LoadingContainer isLoading={true}>
+        <div>Child</div>
+      </LoadingContainer>
+    );
+
+    component.unmount();
+
+    const clearTimeoutSpy = jest.spyOn(window, "clearTimeout");
+    expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
+  });
 });
