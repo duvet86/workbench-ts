0568fae0e4658e7ea3ccf7105b4cbde8703807c9
Luca <duvet86@gmail.com>
2018-10-24 22:34:08 +0800

    Wortking.

--- src/workbench/query/constraintSelector/epic.ts
+++ src/workbench/query/constraintSelector/epic.ts
@@ -23,55 +23,57 @@ import { getAllowedValuesForForFilterObs } from "workbench/query/constraintSelec
 
 import { RootState } from "rootReducer";
 
-// export const addConstraintUpdateGraphEpic = (
-//   action$: ActionsObservable<Action>
+export const requestAllowedValuesEpic = (action$: ActionsObservable<Action>) =>
+  action$.pipe(
+    ofType<Action, IAddQueryConstraint>(
+      QueryConstraintActionTypes.QUERY_CONSTRAINT_ADD
+    ),
+    map(({ constraint: { FilterName } }) => allowedValuesRequest(FilterName!))
+  );
+
+// export const requestAllowedValuesEpic = (
+//   action$: ActionsObservable<Action>,
+//   state$: StateObservable<RootState>
 // ) =>
 //   action$.pipe(
-//     ofType<Action, IAddQueryConstraint>(
-//       QueryConstraintActionTypes.QUERY_CONSTRAINT_ADD
+//     filter(
+//       (action): action is IAddQueryConstraint =>
+//         action.type === QueryConstraintActionTypes.QUERY_CONSTRAINT_ADD &&
+//         (action as IAddQueryConstraint).constraint.FilterName != null
 //     ),
-//     map(({ constraint: { FilterName } }) => allowedValuesRequest(FilterName!))
+//     mergeMap(({ constraint: { FilterName } }) => {
+//       const {
+//         sessionReducer: { session, graph, queries, filters, connections }
+//       } = state$.value;
+
+//       if (session == null || graph == null) {
+//         throw new Error(
+//           "serviceDescriptionEpic: session or graph cannot be null."
+//         );
+//       }
+
+//       graph.Limit = "SaveOnly";
+//       graph.Type = "Partial";
+
+//       const denormalizedGraph = denormalize(graph, graphSchema, {
+//         queries,
+//         filters,
+//         connections
+//       });
+
+//       const { TenantId, SessionId, QueryGraphId } = session;
+//       return saveGraphObs(
+//         TenantId,
+//         SessionId,
+//         QueryGraphId,
+//         denormalizedGraph,
+//         true
+//       ).pipe(
+//         map(() => allowedValuesRequest(FilterName!)),
+//         catchError(error => handleException(error))
+//       );
+//     })
 //   );
-
-export const requestAllowedValuesEpic = (
-  action$: ActionsObservable<Action>,
-  state$: StateObservable<RootState>
-) =>
-  action$.pipe(
-    filter(
-      (action): action is IAddQueryConstraint =>
-        action.type === QueryConstraintActionTypes.QUERY_CONSTRAINT_ADD &&
-        (action as IAddQueryConstraint).constraint.FilterName != null
-    ),
-    mergeMap(({ constraint: { FilterName } }) => {
-      const {
-        sessionReducer: { session, graph, queries, filters, connections }
-      } = state$.value;
-
-      if (session == null || graph == null) {
-        throw new Error(
-          "serviceDescriptionEpic: session or graph cannot be null."
-        );
-      }
-
-      const denormalizedGraph = denormalize(graph, graphSchema, {
-        queries,
-        filters,
-        connections
-      });
-
-      const { TenantId, SessionId, QueryGraphId } = session;
-      return saveGraphObs(
-        TenantId,
-        SessionId,
-        QueryGraphId,
-        denormalizedGraph
-      ).pipe(
-        map(() => allowedValuesRequest(FilterName!)),
-        catchError(error => handleException(error))
-      );
-    })
-  );
 
 export const getAllowedValuesEpic = (
   action$: ActionsObservable<Action>,
