cedb8eb2e21fad0d3a2f06c22351b9146f210694
Luca Marangon <Luca_Marangon@Trimble.com>
2018-11-05 12:10:27 +0800

    Working.

---
+++ copyPublicFolder.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const fs = require("fs-extra");
+
+function copyPublicFolder() {
+  fs.copySync(
+    path.resolve(__dirname, "public"),
+    path.resolve(__dirname, "build"),
+    {
+      dereference: true,
+      filter: file => file !== "index.html"
+    }
+  );
+}
+
+copyPublicFolder();
